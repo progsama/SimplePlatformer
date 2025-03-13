fc7a2516f701f2d3cc00f3b5c0e5917ea6861795
progsama <imtiaznasif@gmail.com>
2025-03-12 17:48:19 -0700

    Added mechanism to delete coins when player touches them

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            Destroy(gameObject);
+        }
+    }
+}
